dab76dc8572d77e2f04e39a692cf37e3a01f504d
kat3005 <kasia.alicja@gmail.com>
2019-09-20 15:58:52 +0100

    Adds completed unit tests

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,4 +11,65 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  it('should add 1 to 4 and get 5', function() {
+    calculator.previousTotal = 4;
+    calculator.add(1);
+    const actual = calculator.runningTotal;
+    assert.equal(actual, 5);
+  })
+
+  it('should subtract 4 from 7 and get 3', function() {
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    const actual = calculator.runningTotal;
+    assert.equal(actual, 3);
+  })
+
+  it('should multiply 3 by 5 and get 15', function() {
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    const actual = calculator.runningTotal;
+    assert.equal(actual, 15);
+  })
+
+  it('should divide 21 by 7 and get 3', function() {
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    const actual = calculator.runningTotal;
+    assert.equal(actual, 3);
+  })
+
+  it('should display concatenated numbers upon clicks', function() {
+    calculator.numberClick(1);
+    calculator.numberClick(4);
+    calculator.numberClick(7);
+    const actual = calculator.runningTotal;
+    assert.equal(actual, 147);
+  })
+
+  it('should chain multiple operations together', function() {
+    calculator.runningTotal = 5;
+    calculator.operatorClick('+');
+    calculator.numberClick(2);
+    calculator.operatorClick('=');
+    const actual = calculator.runningTotal;
+    assert.equal(actual, 7);
+  })
+
+  it('should clear the running total without affecting the calculation', function(){
+    calculator.runningTotal = 5;
+    calculator.operatorClick('+');
+    calculator.numberClick(2);
+    calculator.clearClick();
+    calculator.numberClick(3);
+    calculator.operatorClick('=');
+    const actual = calculator.runningTotal;
+    assert.equal(actual, 8);
+  })
+  
+
+
+
+
+
 });
--- react_prep/hw_es6_instruments_start/client/src/views/select_view.js
+++ react_prep/hw_es6_instruments_start/client/src/views/select_view.js
@@ -4,28 +4,28 @@ class SelectView {
   constructor(element) {
     this.element = element;
   }
+  
+  bindEvents() {
+    PubSub.subscribe('InstrumentFamilies:data-ready', (evt) => {
+      const allInstrumentFamilies = evt.detail;
+      this.populate(allInstrumentFamilies);
+    });
 
-    bindEvents() {
-      PubSub.subscribe('InstrumentFamilies:data-ready', (evt) => {
-        const allInstrumentFamilies = evt.detail;
-        this.populate(allInstrumentFamilies);
-      });
+    this.element.addEventListener('change', (evt) => {
+      const selectedIndex = evt.target.value;
+      PubSub.publish('SelectView:change', selectedIndex);
+    });
+  };
 
-      this.element.addEventListener('change', (evt) => {
-        const selectedIndex = evt.target.value;
-        PubSub.publish('SelectView:change', selectedIndex);
-      });
-    };
-
-    populate(instrumentFamilyData) {
-      instrumentFamilyData.forEach((familiy, index) => {
-        const option = document.createElement('option');
-        option.textContent = familiy.name;
-        option.value = index;
-        this.element.appendChild(option);
-      });
-    };
-  }
+  populate(instrumentFamilyData) {
+    instrumentFamilyData.forEach((familiy, index) => {
+      const option = document.createElement('option');
+      option.textContent = familiy.name;
+      option.value = index;
+      this.element.appendChild(option);
+    });
+  };
+}
 
 
 export default SelectView;
